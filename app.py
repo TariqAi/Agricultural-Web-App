24055ac91d3b1c902f297f94e520dd182df84a7f
Tariq Ibrahim <145915009+TariqAi@users.noreply.github.com>
2024-05-16 14:22:07 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pickle
+import streamlit as st
+
+st.markdown("<h2 style='text-align: center; color: green;'>Agricultural land Web app</h2>", unsafe_allow_html=True)
+
+# loading the saved model
+model = pickle.load(open('model.sav', 'rb'))
+
+# function to convert input data to numeric values
+def convert_to_float(value):
+    try:
+        return float(value)
+    except ValueError:
+        return np.nan
+
+# getting the input data from the user and converting to numeric values
+N = convert_to_float(st.text_input('N'))
+P = convert_to_float(st.text_input('P'))
+K = convert_to_float(st.text_input('K'))
+temperature = convert_to_float(st.text_input('Temperature'))
+humidity = convert_to_float(st.text_input('Humidity'))
+ph = convert_to_float(st.text_input('Ph'))
+rainfall = convert_to_float(st.text_input('Rainfall'))
+
+# code for Prediction
+diagnosis = ''
+
+# creating a button for Prediction
+if st.button('Result'):
+    # Check if any of the input values are NaN (non-numeric)
+    if np.isnan([N, P, K, temperature, humidity, ph, rainfall]).any():
+        st.error("Please provide numeric values for all input fields.")
+    else:
+        # Convert input values to array and reshape for prediction
+        input_data = np.array([N, P, K, temperature, humidity, ph, rainfall]).reshape(1, -1)
+        prediction = model.predict(input_data)
+
+        # Mapping the prediction to the crop names
+        crop_names = [
+            "Rice", "Maize", "Jute", "Cotton", "Coconut", "Papaya", "Orange", "Apple",
+            "Muskmelon", "Watermelon", "Grapes", "Mango", "Banana", "Pomegranate", 
+            "Lentil", "Blackgram", "Mungbean", "Mothbeans", "Pigeonpeas", "Kidneybeans", 
+            "Chickpea", "Coffee"
+        ]
+        diagnosis = crop_names[prediction[0]]
+        
+        st.success(f"It is {diagnosis}")
+
+st.markdown("<h4 style='text-align: center; color: red;'> Founded by Rim Abria </h4>", unsafe_allow_html=True)
